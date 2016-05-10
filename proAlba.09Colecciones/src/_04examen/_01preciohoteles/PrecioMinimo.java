4e43e8ec9724f6784d496e8e9f9b760b215aec69
alumno <alumno@alumno-desktop>
2016-05-10 09:16:57 +0200

    9

---
+++ proAlba.09Colecciones/src/_04examen/_01preciohoteles/PrecioMinimo.java
@@ -0,0 +1,104 @@
+package _04examen._01preciohoteles;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class PrecioMinimo {
+	public static void main(String[] args) {
+		Collection<Hotel> c = new ArrayList<>();
+
+		c.add(new Hotel("A", 20));
+		c.add(new Hotel("B", 10));
+		c.add(new Hotel("C", 30));
+		c.add(new Hotel("A", 18));
+		c.add(new Hotel("B", 21));
+		c.add(new Hotel("B", 22));
+
+		precioMinimoDeHoteles(c);
+		
+		System.out.println();
+		
+		precioMinimoDeHoteles2(c);
+		
+		Collection<Hotel> c2 = new ArrayList<>();
+
+		c2.add(new Hotel("A", 20));
+		c2.add(new Hotel("B", 10));
+		c2.add(new Hotel("C", 30));
+		c2.add(new Hotel("A", 18));
+		c2.add(new Hotel("B", 21));
+		c2.add(new Hotel("B", 22));
+		c2.add(new Hotel("A", 21));
+		c2.add(new Hotel("C", 22));
+		
+		System.out.println("Por Precio: ");
+		hotelesPorPrecio(c2);
+
+	}
+
+	public static void precioMinimoDeHoteles(Collection<Hotel> c) {
+		TreeMap<String, Double> h = new TreeMap<>();
+		for (Hotel hotel : c) {
+			if (!h.containsKey(hotel.getNombre())) {
+				h.put(hotel.getNombre(), hotel.getPrecio());
+			} else if (h.get(hotel.getNombre()) > hotel.getPrecio()) {
+				h.replace(hotel.getNombre(), hotel.getPrecio());
+			}
+		}
+		
+		System.out.println(h.toString());
+	}
+	
+	
+	//Correccion Javi
+	public static void precioMinimoDeHoteles2(Collection<Hotel> c){
+		Map <String,Double> m = new HashMap<>();
+		//Recorremos la coleccion
+		for(Hotel h : c){
+			if (!m.containsKey(h.getNombre())) {
+				m.put(h.getNombre(), h.getPrecio());
+			} else {
+				double precioMinimo = m.get(h.getNombre());
+				if(h.getPrecio() < precioMinimo) {
+					m.put(h.getNombre(), h.getPrecio());
+				}
+			}
+		}
+		//Mostrar los resultados
+		Set<String> nombres = m.keySet();
+		TreeSet<String> nombresOrdenados = new TreeSet<> (nombres);
+		for(String n : nombresOrdenados){
+			System.out.println("Hotel: " + n + " mejor precio: " + m.get(n));
+		}
+	}
+	
+	public static void hotelesPorPrecio(Collection <Hotel> c){
+		Map <Double,List<String>> m = new HashMap<>();
+		//Recorremos la coleccion
+		for(Hotel h : c){
+			if (!m.containsKey(h.getPrecio())) {
+				List<String> l = new LinkedList<>();
+				l.add(h.getNombre());
+				m.put(h.getPrecio(), l);
+			} else {
+				List<String> l = m.get(h.getPrecio());
+				l.add(h.getNombre());
+				m.replace(h.getPrecio(), l);
+				}
+			}
+		//Mostrar los resultados
+		Set<Double>numeros = m.keySet();
+		TreeSet<Double> num = new TreeSet<>(numeros);
+		for(Double n : num){
+			System.out.println("Precio: " + n + " hoteles: " + m.get(n));
+		}
+	}
+
+}
